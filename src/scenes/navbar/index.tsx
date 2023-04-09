e1bf3e7ed5d6b8d246d231d2c7e447d81c0dc051
bengin34 <becaglar3434@gmail.com>
2023-04-09 21:58:56 +0300

    Link

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -27,6 +27,7 @@ const Navbar = (props: Props) => {
                         </div>
                         <div className={`${flexBetween} gap-8`} >
                             <p>Sign In</p>
+                            <button>Become a Member</button>
                         </div>
                     </div>
                 </div>
