25cd5ca08ec8934d58fc50c8b0d76cd132885cdb
bengin34 <becaglar3434@gmail.com>
2023-04-11 02:26:46 +0300

    navbar

--- src/shared/ActionButton.tsx
+++ src/shared/ActionButton.tsx
@@ -8,7 +8,8 @@ type Props = {
 
 const ActionButton = ({ children, setSelectedPage }: Props) => {
   return <AnchorLink className="rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white"
-  onClick={() => setSelectedPage(SelectedPage.ContactUs )}
+  onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+  href={`#${SelectedPage.ContactUs}`}
   >{children}</AnchorLink>;
 };
 
