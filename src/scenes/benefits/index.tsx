94df59ab0b7b42ced1736fec3eef43f502fc13f2
bengin34 <becaglar3434@gmail.com>
2023-04-17 12:09:39 +0300

    scrol

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -15,19 +15,19 @@ const benefits: Array<BenefitType> = [
     icon: <HomeModernIcon className="h-6 w-6" />,
     title: "State of the Art Facilities",
     description:
-      "Neque adipiscing amet amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et.",
+      "At our fitness center, we are committed to providing our members with the best facilities and equipment to help them achieve their fitness goals.Our state-of-the-art facilities are designed to cater to all levels of fitness enthusiasts.",
   },
   {
     icon: <UserGroupIcon className="h-6 w-6" />,
     title: "100's of Diverse Classes",
     description:
-      "Eu ipsum id egestas risus tempus enim semper felis quis. Nec consectetur ac venenatis facilisi est. Eget ac turpis id.",
+      "Our classes range from high-intensity cardio to calming yoga, and everything in between. Our experienced instructors will guide you through each class, ensuring proper form and technique for maximum effectiveness and safety.",
   },
   {
     icon: <AcademicCapIcon className="h-6 w-6" />,
     title: "Expert and Pro Trainers",
     description:
-      "Fusce vestibulum aliquam ut cras. Nisl lectus egestas sapien nisl. Lacus at mi sit pellentesque. Congue parturient.",
+      "We believe that having a knowledgeable and experienced team of trainers is key to helping our members achieve their fitness goals. That's why we only hire the best of the best.",
   },
 ];
 
@@ -87,9 +87,13 @@ const Benefits = ({ setSelectedPage }: Props) => {
           ))}
         </motion.div>
         {/*  Graphics and descr */}
-        <div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex" >
+        <div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
           {/* graphic */}
-          <img className="mx-auto"  src={BenefitsPageGraphic} alt="benefits-page-graphic" />
+          <img
+            className="mx-auto"
+            src={BenefitsPageGraphic}
+            alt="benefits-page-graphic"
+          />
 
           {/* desc */}
           <div>
@@ -97,56 +101,54 @@ const Benefits = ({ setSelectedPage }: Props) => {
             <div className="relative">
               <div className="before:absolute before:-left-20 before:-top-20 before:z-[1] before:content-abstractwaves">
                 <motion.div
-                 initial="hidden"
-                 whileInView="visible"
-                 viewport={{ once: true, amount: 0.5 }}
-                 transition={{ delay: 0.2, duration: 1 }}
-                 variants={{
-                   hidden: { opacity: 0, x: 50 },
-                   visible: { opacity: 1, x: 0 },
-                 }}
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true, amount: 0.5 }}
+                  transition={{ delay: 0.2, duration: 1 }}
+                  variants={{
+                    hidden: { opacity: 0, x: 50 },
+                    visible: { opacity: 1, x: 0 },
+                  }}
                 >
                   <HText>
-                    MILLIONS OF HAPPY MEMBERS GETTING
-                    <span className="text-primary-500"> FIT</span>
+                    JOIN OUR FITNESS COMMUNITY WITH MILLIONS OF
+                    <span className="text-primary-500"> HAPPY MEMBERS</span>
                   </HText>
                 </motion.div>
               </div>
             </div>
             {/* desc */}
             <motion.div
-             initial="hidden"
-             whileInView="visible"
-             viewport={{ once: true, amount: 0.5 }}
-             transition={{ delay: 0.2, duration: 1 }}
-             variants={{
-               hidden: { opacity: 0, x: 50 },
-               visible: { opacity: 1, x: 0 },
-             }}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ delay: 0.2, duration: 1 }}
+              variants={{
+                hidden: { opacity: 0, x: 50 },
+                visible: { opacity: 1, x: 0 },
+              }}
             >
-              <p className="my-5" >
-                Nascetur aenean massa auctor tincidunt. Iaculis potenti amet
-                egestas ultrices consectetur adipiscing ultricies enim. Pulvinar
-                fames vitae vitae quis. Quis amet vulputate tincidunt at in
-                nulla nec. Consequat sed facilisis dui sit egestas ultrices
-                tellus. Ullamcorper arcu id pretium sapien proin integer nisl.
-                Felis orci diam odio.
+              <p className="my-5">
+                Are you looking for a way to get fit and healthy? Look no
+                further than our fitness community with millions of happy
+                members! We provide a range of services to help you achieve your
+                fitness goals.
               </p>
-              <p className="mb-5" >
-                Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
-                tellus quam porttitor. Mauris velit euismod elementum arcu neque
-                facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit
-                enim mattis odio in risus nunc.
+              <p className="mb-5">
+                Our experienced trainers will guide you through a range of
+                exercises to help you get the most out of your workouts. From
+                weightlifting to cardio, we've got you covered. Our facilities
+                are equipped with state-of-the-art equipment to ensure you have
+                everything you need to succeed.
               </p>
             </motion.div>
             {/* buttton */}
-            <div className="relative mt-16" >
-              <div className="befıre:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles" >
-                <ActionButton setSelectedPage={setSelectedPage} >
+            <div className="relative mt-16">
+              <div className="befıre:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
+                <ActionButton setSelectedPage={setSelectedPage}>
                   Join Now
                 </ActionButton>
               </div>
-
             </div>
           </div>
         </div>
