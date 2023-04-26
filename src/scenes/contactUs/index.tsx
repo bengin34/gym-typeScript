165222c14e560b7ae918bbc0a6d77e23890be466
bengin34 <becaglar3434@gmail.com>
2023-04-26 18:09:51 +0300

    contact

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -1,10 +1,19 @@
+import { SelectedPage } from "@/shared/types"
+import {motion} from "framer-motion"
+import { useForm } from "react-hook-form"
+import ContactUsPageGraphic from "../../assets/ContactUsPageGraphic.png"
 
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void
+}
 
-type Props = {}
-
-const ContactUs = (props: Props) => {
+const ContactUs = ({setSelectedPage}: Props) => {
   return (
-    <div>ContactUs</div>
+    <section id="contactus" className="mx-auto w-5/6 pt-24 pb-32" >
+      <motion.div onView >
+
+      </motion.div>
+    </section>
   )
 }
 
