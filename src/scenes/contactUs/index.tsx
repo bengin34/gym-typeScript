130434173f429518d2a74e23131a23ae3037a6f7
bengin34 <becaglar3434@gmail.com>
2023-04-28 02:09:00 +0300

    contact

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -1,20 +1,141 @@
-import { SelectedPage } from "@/shared/types"
-import {motion} from "framer-motion"
-import { useForm } from "react-hook-form"
-import ContactUsPageGraphic from "../../assets/ContactUsPageGraphic.png"
+import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
+import { useForm } from "react-hook-form";
+import ContactUsPageGraphic from "../../assets/ContactUsPageGraphic.png";
+import HText from "@/shared/HText";
 
 type Props = {
-  setSelectedPage: (value: SelectedPage) => void
-}
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-const ContactUs = ({setSelectedPage}: Props) => {
+const ContactUs = ({ setSelectedPage }: Props) => {
+  const inputStyles = `w-full mt-5 rounded-lg bg-primary-300 px-5 py-3 placeholder-white`;
+
+  const {
+    register,
+    trigger,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (e: any) => {
+    const isValid = await trigger();
+    if (!isValid) {
+      e.preventDefault();
+    }
+  };
+
   return (
-    <section id="contactus" className="mx-auto w-5/6 pt-24 pb-32" >
-      <motion.div onView >
+    <section id="contactus" className="mx-auto w-5/6 pb-32 pt-24">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
+      >
+        {/* HEADER */}
+        <motion.div
+          className="md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 1 }}
+          variants={{
+            hidden: { opacity: 0, x: -150 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <HText>
+            <span className="text-primary-500">JOIN NOW</span> TO GET IN SHAPE
+          </HText>
+          <p className="my-5">
+            With our program, you can achieve your fitness goals and get in
+            shape in no time. Whether you want to lose weight, gain muscle, or
+            just improve your overall health, our app has everything you need to
+            succeed. Our team of expert trainers will guide you through
+            personalized workouts and nutrition plans, while our community of
+            users will motivate and inspire you along the way. So what are you
+            waiting for? Join now and start your fitness journey today!
+          </p>
+        </motion.div>
 
+        {/* FORM AND IMAGE */}
+        <div className="mt-10 justify-between gap-8 md:flex">
+          <motion.div
+            className="mt-10 basis-3/5 md:mt-0"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 1 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+          >
+            <form
+              target="_blank"
+              onSubmit={onSubmit}
+              action="https://formsubmit.co/becaglar3434@gmail.com"
+              method="POST"
+            >
+              <input
+                className={inputStyles}
+                type="text"
+                placeholder="Name"
+                {...register("name", {
+                  required: true,
+                  maxLength: 100,
+                })}
+              />
+              {errors.name && (
+                <p className="mt-1 text-primary-500">
+                  {errors.name.type === "required" && "This field is required"}
+                  {errors.name.type === "maxLength" &&
+                    "Max lenght is 100 char."}
+                </p>
+              )}
+
+              <input
+                className={inputStyles}
+                type="text"
+                placeholder="EMAIL"
+                {...register("email", {
+                  required: true,
+                  pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                })}
+              />
+              {errors.email && (
+                <p className="mt-1 text-primary-500">
+                  {errors.email.type === "required" &&
+                    "This field is required."}
+                  {errors.email.type === "pattern" && "Invalid email address."}
+                </p>
+              )}
+
+              <textarea
+                className={inputStyles}
+                placeholder="MESSAGE"
+                rows={4}
+                cols={50}
+                {...register("message", {
+                  required: true,
+                  maxLength: 2000,
+                })}
+              />
+              {errors.message && (
+                <p className="mt-1 text-primary-500">
+                  {errors.message.type === "required" &&
+                    "This field is required."}
+                  {errors.message.type === "maxLength" &&
+                    "Max length is 2000 char."}
+                </p>
+              )}
+
+              <button type="submit" className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"  > 
+              SUBMIT
+              </button>
+            </form>
+          </motion.div>
+        </div>
       </motion.div>
     </section>
-  )
-}
+  );
+};
 
-export default ContactUs
+export default ContactUs;
